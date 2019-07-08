621fe8ad0f29adadd235c2a98181e995c48bbda1
doolb <xue.blood@outlook.com>
2019-07-08 11:41:29 +0800

    add multi command support

--- addon.py
+++ addon.py
@@ -4,7 +4,19 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-cmd = addon.getSetting("cmd")
+cmd = addon.getSetting("cmd").split(';')
+names = []
+cmds = []
+for d in cmd:
+    one = d.split(',')
+    names.append(one[0])
+    cmds.append(one[-1])
 
-if(xbmcgui.Dialog().yesno(addonname, "Run command?", cmd)):
+if(len(names) > 1):
+    choice = xbmcgui.Dialog().select("Select Command:", names)
+    name = names[choice]
+    cmd = cmds[choice]
+#xbmcgui.Dialog().ok("You choice:","11")
+
+if(xbmcgui.Dialog().yesno(addonname, "Run command?", name, cmd)):
     xbmc.executebuiltin('System.Exec(' + cmd + ')')
